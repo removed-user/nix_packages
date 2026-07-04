72441bc26976be8f95cbbc767b343db269c703b7
casey <caseyriley94@gmail.com>
2026-07-04 02:47:45 -0400

    tests/bump

--- flake.nix
+++ flake.nix
@@ -7,11 +7,41 @@
   };
 
   outputs = inputs @ {
-    flake-parts,
+    # flake-parts-lib,
     nixpkgs,
+    self,
     ...
-  }:
-    flake-parts.lib.mkFlake {
+  }: let
+    _module.args.system = "x86_64-linux";
+    system = _module.args.system;
+    lib = nixpkgs.lib;
+    flake-parts-lib = inputs.flake-parts.lib;
+    # _module.args.pkgs = pkgs;
+    specialArgs = {
+      system = "x86_64-linux";
+    };
+    ###
+    pkgs = _module.args.pkgs;
+    ###
+    _module.args.pkgs = {
+      inputs,
+      system ? specialArgs.system,
+      ...
+    }: {
+      imports = [./pkgs/main.nix];
+    };
+
+    # stdenv = _module.args.stdenv;
+    # _module.args.stdenv = {
+    #   inputs,
+    #   system ? specialArgs.system,
+    #   ...
+    # }: {
+    #   imports = [./pkgs/stdenv.nix];
+    # };
+    config.debug = true;
+  in
+    flake-parts-lib.mkFlake {
       inherit inputs;
     } {
       imports = [
@@ -19,6 +49,7 @@
         inputs.flake-parts.flakeModules.modules
         inputs.flake-parts.flakeModules.debug
         inputs.flake-parts.flakeModules.partitions
+        inputs.flake-parts.flakeModules.easyOverlay
       ];
 
       disabledModules = [
@@ -30,27 +61,18 @@
       ];
 
       perSystem = let
-        config.strictDepsByDefault = true;
-        lib = nixpkgs.lib;
-
-        system = "x86_64-linux";
+        bootStrap = {pkgs, ...}: lib.recurseIntoAttrs pkgs.minimal-bootstrap;
+        # inputs.nixpkgs.config.replaceStdenv = inputs'.nixpkgs.legacyPackages.minimal-bootstrap;
       in
         {
           config,
-          self',
+          # self',
           inputs',
           pkgs,
-          system',
+          system,
           ...
-        }: let
-          bootStrap = lib.recurseIntoAttrs inputs'.nixpkgs.legacyPackages.minimal-bootstrap;
-          # inputs.nixpkgs.config.replaceStdenv = inputs'.nixpkgs.legacyPackages.minimal-bootstrap;
-        in {
-          # Per-system attributes can be defined here. The self' and inputs'
-          # module parameters provide easy access to attributes of the same
-          # system.
-
-          legacyPackages = {
+        }: {
+          self.legacyPackages = {
             inherit bootStrap;
             list = builtins.attrNames bootStrap;
             guix = inputs'.nixpkgs.legacyPackages.guix;
@@ -62,7 +84,7 @@
           };
         };
       flake = {
-        config.strictDepsByDefault = true;
+        # config.strictDepsByDefault = true;
         # The usual flake attributes can be defined here, including system-
         # agnostic ones like nixosModule and system-enumerating ones, although
         # those are more easily expressed in perSystem.
